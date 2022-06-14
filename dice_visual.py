9480946c9b0eef664ef9a0162abc4ab958e7d061
SkymanSky <gokera@ogr.iu.edu.tr>
2022-06-14 23:02:26 +0300

    50000 kez atılan 6 ve 10 yüzlü  zarların toplamlarının histogramı oluşturuldu.

--- dice_visual.py
+++ dice_visual.py
@@ -6,11 +6,11 @@ from die import Die
 #D6 oluştur.
 
 die_1=Die()
-die_2=Die()
+die_2=Die(10)
 
 #Bazı atışlar yap ve sonucu bir listede sakla.
 results=[]
-for roll_num in range(1000):
+for roll_num in range(50000):
     result=die_1.roll()+die_2.roll()
     results.append(result)
 
@@ -27,7 +27,7 @@ x_values=list(range(2,max_result+1))
 data= [Bar(x=x_values,y=frequencies)]
 x_axis_config={'title':'Sonuç','dtick':1}
 y_axis_config={'title':'Sonucun Frekansı'}
-my_layout=Layout(title='İki D6 yı 1000 kez atmanın sonuçları',xaxis=x_axis_config,yaxis=y_axis_config)
-offline.plot({'data':data,'layout':my_layout},filename='d6_d6.html')
+my_layout=Layout(title='Bir D6 ve D10 u 50000 kez atmanın sonuçları',xaxis=x_axis_config,yaxis=y_axis_config)
+offline.plot({'data':data,'layout':my_layout},filename='d6_d10.html')
 
 #print(frequencies)
